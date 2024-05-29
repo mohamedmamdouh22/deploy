09a5841747d7ca7767c66108699fc36b3d7a3a89
omar-mostafa-taha <omar195628@feng.bu.edu.eg>
2024-05-29 22:21:57 +0300

    modified globals

--- globals.py
+++ globals.py
@@ -1,5 +1,12 @@
+from torchvision import transforms
+
 processing_status = {'status': 'idle'}
-y_length = 256
-x_length = 256
-n_mean = [0.5, 0.5, 0.5]
-n_std = [0.5, 0.5, 0.5]
+
+# Transform to be applied to each image
+resize_dims = (256, 256)
+n_mean_std = [0.5, 0.5, 0.5]
+data_transform = transforms.Compose([
+    transforms.Resize(resize_dims, antialias=True),
+    transforms.ToTensor(),
+    transforms.Normalize(n_mean_std, n_mean_std),
+])
