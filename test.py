fd22b443b31236ce8b59dc0533c7ce5e0d499ddb
omar-mostafa-taha <omar195628@feng.bu.edu.eg>
2024-05-31 00:42:49 +0300

    yolo batch inference

---
+++ test.py
@@ -0,0 +1,21 @@
+import time
+from utils import *
+
+# example
+top_left = (75, 200)  # Replace with your top-left coordinates
+bottom_right = (1205, 600)  # Replace with your bottom-right coordinates
+skip_frames, min_width, min_height, batch_size = 3 , 50 , 80 , 32
+video_path = 'video.mp4'
+
+model, yolo = load_models()
+
+frames = extract_frames(video_path, 3)
+print("len frames: ", len(frames))
+
+start_time = time.time()
+emb = detect_objects(yolo, frames, top_left, bottom_right, min_width=50, min_height=80, batch_size=8)
+end_time = time.time()
+
+print("detection time is: ",end_time - start_time)
+
+
